ed13cbcbdfaa8bbdd64efdf63f16c36b5bd6b1e5
Pavel Harmaza <pavelharmaza@gmail.com>
2022-03-10 01:56:30 +0300

    feat: add endpoints for userpage

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -26,14 +26,25 @@ authRouter.post(
       if (candidate) {
         return res.status(200).json({ message: "User already exist" });
       }
+      const emailUnique = await AuthModel.findOne({ email });
+      if (emailUnique) {
+        return res.status(200).json({ message: "Email is taken" });
+      }
       const hashedPassword = await bcrypt.hash(password, 12);
+
       const user = await AuthModel.create({
         email,
         username,
         password: hashedPassword,
       });
       await user.save();
-      res.status(201).json({ message: "User created" });
+      const token = jwt.sign({ username: user.username }, "nepravda", {
+        expiresIn: "1h",
+      });
+      return res.status(201).json({
+        message: "User created",
+        token: token,
+      });
     } catch (e) {
       console.error(e.message);
       res
@@ -53,7 +64,7 @@ authRouter.post(
   async (req, res) => {
     try {
       const errors = validationResult(req);
-      console.log(errors);
+      // console.log(errors);
       if (!errors.isEmpty()) {
         return res.status(400).json({ message: "Incorrect login data" });
       }
@@ -66,10 +77,10 @@ authRouter.post(
       if (!isMatch) {
         return res.status(200).json({ message: "Incorrect email or password" });
       }
-      const token = jwt.sign({ userId: user._id }, "nepravda", {
+      const token = jwt.sign({ username: user.username }, "nepravda", {
         expiresIn: "1h",
       });
-      return res.json(token);
+      return res.json({ token: token });
     } catch (e) {
       console.error(e);
       res
@@ -78,5 +89,143 @@ authRouter.post(
     }
   }
 );
+authRouter.get("/getuser/:username", async (req, res) => {
+  try {
+    const username = req.params;
+
+    const user = await AuthModel.findOne(username);
+    return res.json({ user: user });
+  } catch (e) {
+    console.error(e.message);
+    return res
+      .status(500)
+      .json({ message: "Something went wrong. Try again later" });
+  }
+});
+authRouter.delete("/deleteuser", async (req, res) => {
+  try {
+    console.log(req.body);
+    const { username, password } = req.body;
+    const user = await AuthModel.findOne({ username: username });
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (isMatch) {
+      user.delete();
+      return res.status(200).json({ message: "User successfully deleted" });
+    } else {
+      return res.status(200).json({ message: "Incorrect  password" });
+    }
+  } catch (e) {
+    console.error(e);
+    return res
+      .status(500)
+      .json({ message: "Something went wrong. Try again later" });
+  }
+});
+authRouter.post("/uptade/password", async (req, res) => {
+  try {
+    console.log("req.body:", req.body);
+    const { username, oldPassword, newPassword } = req.body;
+    const user = await AuthModel.findOne({ username });
+    // console.log("user: ", user);
+    const isMatch = await bcrypt.compare(oldPassword, user.password);
+    if (!isMatch) {
+      return res.status(200).json({ message: "Incorrect old password" });
+    } else {
+      const hashedNewPassword = await bcrypt.hash(newPassword, 12);
+      const checkich = await AuthModel.updateOne(
+        { username },
+        { $set: { password: hashedNewPassword } }
+      );
+
+      return res.status(200).json({ message: "Password changed" });
+    }
+  } catch (e) {
+    console.error(e);
+    return res
+      .status(500)
+      .json({ message: "Something went wrong. Try again later" });
+  }
+});
+authRouter.post("/uptade/info", async (req, res) => {
+  try {
+    console.log("req.body:", req.body);
+    const { id, username, email, description } = req.body;
+    const user = await AuthModel.findOne({ id });
+    console.log(user.email, " and ", user.username);
+    const emailCheck = await AuthModel.findOne({ email });
+    const usernameCheck = await AuthModel.findOne({ username });
+    if (emailCheck && usernameCheck) {
+      if (user.id != emailCheck.id) {
+        return res.status(200).json({ message: "Email is taken" });
+      } else if (user.id != usernameCheck.id) {
+        return res.status(200).json({ message: "Username is taken" });
+      } else {
+        const updateAll = await AuthModel.updateOne(
+          { id },
+          {
+            $set: {
+              username: username,
+              email: email,
+              description: description,
+            },
+          }
+        );
+        return res.status(200).json({ message: "Information updated!" });
+      }
+    } else if (emailCheck) {
+      console.log("Email check: ", emailCheck.email);
+      if (user.id != emailCheck.id) {
+        return res.status(200).json({ message: "Email is taken" });
+      } else {
+        const updateAll = await AuthModel.updateOne(
+          { id },
+          {
+            $set: {
+              username: username,
+              email: email,
+              description: description,
+            },
+          }
+        );
+        return res.status(200).json({ message: "Information updated!" });
+      }
+    }
+    if (usernameCheck) {
+      console.log("name check: ", usernameCheck.username);
+      if (user.id != usernameCheck.id) {
+        return res.status(200).json({ message: "Username is taken" });
+      } else {
+        const updateAll = await AuthModel.updateOne(
+          { id },
+          {
+            $set: {
+              username: username,
+              email: email,
+              description: description,
+            },
+          }
+        );
+        return res.status(200).json({ message: "Information updated!" });
+      }
+    } else {
+      const updateAll = await AuthModel.updateOne(
+        { id },
+        {
+          $set: {
+            username: username,
+            email: email,
+            description: description,
+          },
+        }
+      );
+      return res.status(200).json({ message: "Information updated!" });
+    }
+  } catch (e) {
+    console.error(e);
+    return res
+      .status(500)
+      .json({ message: "Something went wrong. Try again later" });
+  }
+});
 
 export default authRouter;
--- routes/models/auth.model.js
+++ routes/models/auth.model.js
@@ -12,6 +12,7 @@ const authSchema = new mongoose.Schema(
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    description: { type: String, required: false },
   },
   { timestamps: true }
 );
